da7aa0e1ed1e11eaace5f22b6824ef0a8c059eef
Yubo Su <yubo@blend.com>
2019-11-18 16:06:18 -0500

    check in comparison of two limits of F maybe?

--- scripts/eccentric_tides/hansens.py
+++ scripts/eccentric_tides/hansens.py
@@ -18,20 +18,59 @@ def hansen_integrand(N, m, e):
     def integrand(E):
         return (
             np.cos(N * (E - e * np.sin(E)) - m * f(E, e)) /
-            (1 - e * np.cos(E))) / (2 * np.pi)
+            (1 - e * np.cos(E))**2) / np.pi
     return integrand
 
+def get_hansen(N, m, e):
+    '''
+    just evaluates the hansen integral (so I don't have to keep typing the
+    bounds everywhere). We need about 2N limits I think.
+    '''
+    integrand = hansen_integrand(N, m, e)
+    return quad(integrand, 0, np.pi, limit=max(100, 2 * N))
+
 if __name__ == '__main__':
+    ms = 2
     m = 2
     e = 0.9
-    print('Ansatz N', (1 - e)**(-3/2))
-    integrand = hansen_integrand(2, m, e)
 
-    E = np.linspace(-np.pi, np.pi, 1000)
-    plt.plot(E, integrand(E), 'r')
-    plt.savefig('hansens.png')
-    # for N in range(1, 100):
-    #     integrand = hansen_integrand(N, m, e)
-    #     y, abserr = quad(integrand, -np.pi, np.pi, limit=100)
-    #     print('%03d, %.8f, %.8e' % (N, y, abserr))
+    N_peak = (1 - e)**(-3/2)
+    print('Ansatz N', N_peak)
 
+    n_vals = np.arange(1, int(30 * N_peak))
+    coeffs = np.zeros_like(n_vals, dtype=np.float64)
+    for idx, N in enumerate(n_vals):
+        y, abserr = get_hansen(N, m, e)
+        coeffs[idx] = y
+        print('%03d, %.8f, %.8e' % (N, y, abserr))
+
+    max_n = np.argmax(np.abs(coeffs))
+    max_c = np.max(np.abs(coeffs))
+    pos_idx = np.where(coeffs > 0)[0]
+    neg_idx = np.where(coeffs < 0)[0]
+    plt.loglog(n_vals[pos_idx], np.abs(coeffs[pos_idx]) / max_c,
+               'ko', ms=ms, label=r'$F_{Nm}$')
+    plt.loglog(n_vals[neg_idx], np.abs(coeffs[neg_idx]) / max_c,
+               'ro', ms=ms)
+    plt.xlabel('N')
+    plt.ylabel(r'$F_{Nm} / F_{Nm,\max}$')
+    plt.axvline(max_n, c='k', linewidth=1)
+    plt.axvline(N_peak, c='b')
+    plt.title(r'$e = %.2f$' % e)
+    plt.tight_layout()
+    plt.savefig('hansens', dpi=400)
+    plt.clf()
+
+    plt.loglog(n_vals[pos_idx],
+               (n_vals**(8/3) * np.abs(coeffs) / max_c)[pos_idx],
+               'kx', ms=ms, label=r'$F_{Nm}N^{8/3}$')
+    plt.loglog(n_vals[neg_idx],
+               (n_vals**(8/3) * np.abs(coeffs) / max_c)[neg_idx],
+               'rx', ms=ms)
+    plt.xlabel('N')
+    plt.ylabel(r'$F_{Nm} N^{8/3} / F_{Nm,\max}$')
+    plt.axvline(max_n, c='k', linewidth=1)
+    plt.axvline(N_peak, c='b')
+    plt.title(r'$e = %.2f$' % e)
+    plt.tight_layout()
+    plt.savefig('hansens_83', dpi=400)
