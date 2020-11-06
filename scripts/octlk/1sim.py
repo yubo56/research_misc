9f11e8edb52199f5b5d127de28409f312b6de256
Yubo Su <yubo@blend.com>
2020-11-06 06:55:03 -0500

    stash

--- scripts/octlk/1sim.py
+++ scripts/octlk/1sim.py
@@ -151,26 +151,26 @@ def sweep(num_trials=3, num_trials_purequad=1, num_i=200, t_hubb_gyr=10,
         # [0.4, 0.9, 'bindist', 70, 110, 10, 700 * np.sqrt(1 - 0.9**2)],
 
         # exploratory, find the right inclination range to restrict to
-        [0.2, 0.6, 'explore_1p2dist', 50, 130, 100, 3600],
-        [0.3, 0.6, 'explore_1p3dist', 50, 130, 100, 3600],
-        [0.4, 0.6, 'explore_1p4dist', 50, 130, 100, 3600],
-        [0.5, 0.6, 'explore_1p5dist', 50, 130, 100, 3600],
-        [0.7, 0.6, 'explore_1p7dist', 50, 130, 100, 3600],
+        # [0.2, 0.6, 'explore_1p2dist', 50, 130, 100, 3600],
+        # [0.3, 0.6, 'explore_1p3dist', 50, 130, 100, 3600],
+        # [0.4, 0.6, 'explore_1p4dist', 50, 130, 100, 3600],
+        # [0.5, 0.6, 'explore_1p5dist', 50, 130, 100, 3600],
+        # [0.7, 0.6, 'explore_1p7dist', 50, 130, 100, 3600],
         [1.0, 0.6, 'explore_1equaldist', 50, 130, 100, 3600],
 
-        [0.2, 0.8, 'explore_e81p2dist', 50, 130, 100, 3600],
-        [0.3, 0.8, 'explore_e81p3dist', 50, 130, 100, 3600],
-        [0.4, 0.8, 'explore_e81p4dist', 50, 130, 100, 3600],
-        [0.5, 0.8, 'explore_e81p5dist', 50, 130, 100, 3600],
-        [0.7, 0.8, 'explore_e81p7dist', 50, 130, 100, 3600],
-        [1.0, 0.8, 'explore_e81equaldist', 50, 130, 100, 3600],
+        # [0.2, 0.8, 'explore_e81p2dist', 50, 130, 100, 3600],
+        # [0.3, 0.8, 'explore_e81p3dist', 50, 130, 100, 3600],
+        # [0.4, 0.8, 'explore_e81p4dist', 50, 130, 100, 3600],
+        # [0.5, 0.8, 'explore_e81p5dist', 50, 130, 100, 3600],
+        # [0.7, 0.8, 'explore_e81p7dist', 50, 130, 100, 3600],
+        # [1.0, 0.8, 'explore_e81equaldist', 50, 130, 100, 3600],
 
-        [0.2, 0.9, 'explore_e91p2dist', 50, 130, 100, 3600],
-        [0.3, 0.9, 'explore_e91p3dist', 50, 130, 100, 3600],
-        [0.4, 0.9, 'explore_e91p4dist', 50, 130, 100, 3600],
-        [0.5, 0.9, 'explore_e91p5dist', 50, 130, 100, 3600],
-        [0.7, 0.9, 'explore_e91p7dist', 50, 130, 100, 3600],
-        [1.0, 0.9, 'explore_e91equaldist', 50, 130, 100, 3600],
+        # [0.2, 0.9, 'explore_e91p2dist', 50, 130, 100, 3600],
+        # [0.3, 0.9, 'explore_e91p3dist', 50, 130, 100, 3600],
+        # [0.4, 0.9, 'explore_e91p4dist', 50, 130, 100, 3600],
+        # [0.5, 0.9, 'explore_e91p5dist', 50, 130, 100, 3600],
+        # [0.7, 0.9, 'explore_e91p7dist', 50, 130, 100, 3600],
+        # [1.0, 0.9, 'explore_e91equaldist', 50, 130, 100, 3600],
 
         # a2 = 4500, e2 = 0.6
         # [0.2, 0.6, '1p2dist', 89.5, 105, 100, 3600],
@@ -179,8 +179,11 @@ def sweep(num_trials=3, num_trials_purequad=1, num_i=200, t_hubb_gyr=10,
         # [0.5, 0.6, '1p5dist', 91, 98, 100, 3600],
         # [0.7, 0.6, '1p7dist', 91, 95, 100, 3600],
         # [1.0, 0.6, '1equaldist', 92.1, 93.5, 100, 3600],
+        # [0.2, 0.6, '1p2distp2', 66, 87, 100, 3600],
+        # [0.3, 0.6, '1p3distp2', 73, 86, 100, 3600],
 
-        # [0.2, 0.8, 'e81p2dist', 89.5, 105, 100, 3600],
+        # [0.2, 0.8, 'e81p2dist', 89, 107, 100, 3600],
+        # [0.2, 0.8, 'e81p2distp2', 57, 86.5, 100, 3600],
         # [0.3, 0.8, 'e81p3dist', 90.5, 100, 100, 3600],
         # [0.4, 0.8, 'e81p4dist', 90.5, 98, 100, 3600],
         # [0.5, 0.8, 'e81p5dist', 91, 98, 100, 3600],
@@ -200,6 +203,11 @@ def sweep(num_trials=3, num_trials_purequad=1, num_i=200, t_hubb_gyr=10,
         a2 = a2eff / np.sqrt(1 - e2**2)
 
         I0s = np.radians(np.linspace(ilow, ihigh, num_i))
+        if q == 1.0:
+            I_plots = np.repeat(I0s, num_trials_purequad)
+        else:
+            I_plots = np.repeat(I0s, num_trials)
+
         fn = '%s/%s' % (folder, base_fn)
         pkl_fn = fn + '.pkl'
         if not os.path.exists(pkl_fn):
@@ -207,11 +215,6 @@ def sweep(num_trials=3, num_trials_purequad=1, num_i=200, t_hubb_gyr=10,
             p = Pool(nthreads)
             m2 = m12 / (1 + q)
             m1 = m12 - m2
-
-            if q == 1.0:
-                I_plots = np.repeat(I0s, num_trials_purequad)
-            else:
-                I_plots = np.repeat(I0s, num_trials)
             args = [
                 (idx, q, t_hubb_gyr * 1e9, a0, a2, e0, e2, I0)
                 for idx, I0 in enumerate(I_plots)
@@ -232,8 +235,8 @@ def sweep(num_trials=3, num_trials_purequad=1, num_i=200, t_hubb_gyr=10,
         merge_probs = []
         for I in I0s:
             merge_probs.append(
-                len(np.where(I_plots[merged] == I)[0]) /
-                len(np.where(I_plots == I)[0]))
+                len(np.where(np.abs(I_plots[merged] - I) < 1e-6)[0]) /
+                len(np.where(np.abs(I_plots - I) < 1e-6)[0]))
         total_merge_fracs.append(
             (ihigh - ilow) / 180 * np.sum(merge_probs) / len(I0s)
         )
@@ -900,8 +903,8 @@ if __name__ == '__main__':
     # emaxes = get_emax_series(0, 1, 92.8146, 2e7)[1]
     # print(1 - np.mean(emaxes))
 
-    # sweep(folder='1sweepbin', nthreads=60)
-    plot_emax_sweep(nthreads=4)
+    # sweep(folder='1sweepbin', nthreads=12)
+    # plot_emax_sweep(nthreads=4)
 
     # plot_emax_dq(I0=93, fn='q_sweep93')
     # plot_emax_dq(I0=93.5, fn='q_sweep_935')
@@ -917,9 +920,10 @@ if __name__ == '__main__':
     # k_sweep()
     pass
 
-    # (Y) do elim(I_0) plot as well
-    # (Y) fix ain, fix abarouteff (quad strength), must change abarouteff when changing
-    # (Y) try to reproduce Bin's gapped case?
-    # (Y) try higher eps_oct?
+    # I think width ~ 1/epsoct^p, probably random walk timescale?
 
-    # TODO: I think width ~ 1/epsoct^p, probably random walk timescale?
+    # exo3 is running bindist
+    # exo2a is running all the rest of the explores
+    # exo15c is running emax_sweeps
+    # exo4 is running 1p2distp2, 1p3distp2, e81p2dist, e81p2distp2
+
